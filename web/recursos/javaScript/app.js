1620255d442f25c268cd1cc24c38b4269951d63c
dsolanoc <doris.solano2017@uteq.edu.ec>
2022-01-03 20:06:23 -0500

    menuA
    cambio de contraste y tamaño de letra

--- web/recursos/javaScript/app.js
+++ web/recursos/javaScript/app.js
@@ -1,43 +1,42 @@
-//black
 const boton = document.querySelector('#boton');
 const configUser = window.matchMedia('(prefers-color-scheme: dark)');
 const localConfig = localStorage.getItem('tema');
+
+   
 if (localConfig === 'dark') {
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('dark-theme');
     
 } else if (localConfig === 'light') {
-    document.body.classList.toggle('light-theme')
+    document.body.classList.toggle('light-theme');
 }
 boton.addEventListener('click', () => {
-    console.log(configUser.matches)
+    console.log(configUser.matches);
     let colorTema;
     if (configUser.matches) {
         // Entramos con modo oscuro
-        document.body.classList.toggle('light-theme')
-        colorTema = document.body.classList.contains('light-theme') ? 'light' : 'dark'
+        document.body.classList.toggle('light-theme');
+        colorTema = document.body.classList.contains('light-theme') ? 'light' : 'dark';
 
     } else {
-        document.body.classList.toggle('dark-theme')
-        colorTema = document.body.classList.contains('dark-theme') ? 'dark' : 'light'
+        document.body.classList.toggle('dark-theme');
+        colorTema = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
     }
-    localStorage.setItem('tema', colorTema)
+    localStorage.setItem('tema', colorTema);
 })
 
 
 
 function tamañoLetra() {
-  size = $(".mitexto" ).css("font-size");
+  size = $(".texto" ).css("font-size");
   size = parseInt(size, 10);
   $( ".tamaño-actual" ).text(size);
 }
 
-
-tamañoLetra();
-
+          
 // Función para disminuir el tamaño del texto (fuente) 
 $(".disminuir").on("click", function() {
   if ((size - 2) >= 13) {
-    $(".mitexto").css("font-size", "-=2");
+    $(".texto").css("font-size", "-=2");
     $(".tamaño-actual").text(size -= 1);
   }
 });
@@ -45,17 +44,24 @@ $(".disminuir").on("click", function() {
 // Función para aumentar el tamaño del texto (fuente) 
 $(".aumentar").on("click", function() { 
   if ((size + 2) <= 47) {
-    $(".mitexto").css("font-size", "+=2");
+    $(".texto").css("font-size", "+=2");
     $(".tamaño-actual").text(size += 1);
   }
 });
 
 // Función para restablecer el tamaño del texto (fuente) al tamaño inicial 
 $(".restablecer").on("click", function() {
-  $(".mitexto").css("font-size", "initial");
-  size = $(".mitexto" ).css("font-size");
+  $(".texto").css("font-size", "initial");
+  size = $(".texto" ).css("font-size");
   size = parseInt(size, 10);
   $( ".tamaño-actual" ).text(size);
 }); 
 
+var ob = document.getElementsByTagName("*");
+          for(var i=0;i<=ob.length;i++)
+          {
+              tamañoLetra();
+              ob[i].classList.add("texto");
+          }
+          
 
